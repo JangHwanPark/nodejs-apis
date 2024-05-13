1a20225cc9ac9cbfa99babcde356320d3ba54da2
JangHwanPark <wkd5878@gmail.com>
2024-05-13 11:04:33 +0900

    init install mysql2, cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
+// 모듈 임포트
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
+// 서버 설정
 const app = express();
 dotenv.config();
 
+
+// 서버 시작
 app.listen(process.env.PORT, () => {
     console.log(`서버 시작 ${process.env.PORT}`)
 })
---
+++ src/utils/db.config.js
@@ -0,0 +1,6 @@
+// 모듈 임포트
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+// 서버 설정
+dotenv.config();
