d9ada4b5c8ffb5db0ffd5c43b15ae929897a919c
JangHwanPark <wkd5878@gmail.com>
2024-05-13 11:17:40 +0900

    init install body-parser, config database(mysql)

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 // 서버 설정
 const app = express();
-dotenv.config();
-
+dotenv.config(); // 환경변수 읽어오기
+app.use(cors()); // 모든 프론트 서버 허용
 
 // 서버 시작
 app.listen(process.env.PORT, () => {
--- src/utils/db.config.js
+++ src/utils/db.config.js
@@ -4,3 +4,12 @@ import dotenv from "dotenv";
 
 // 서버 설정
 dotenv.config();
+
+// 데이터베이스 연동
+const dbConfig =  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+}
