fd2fe57dca4a10c286920d4f5c6aa8402ecdc419
user <wkd5878@gmail.com>
2024-06-06 22:32:36 +0900

    feat: github api 를 활용한 데이터 요청 구현

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import bodyParser from "body-parser";
 
 // 라우터 임포트
 import booksRouter from "./routes/products/books.js";
@@ -10,6 +9,10 @@ import signInRouter from "./routes/auth/login.js";
 import signUpRouter from "./routes/auth/register.js";
 import adminRouter from "./routes/admin.js";
 import blogRouter from "./routes/posts.js";
+import githubRouter from "./routes/github/issue.js";
+
+// 미들웨어 임포트
+import errorHandler from "./middlewares/errorHandler.js";
 
 // 서버 설정
 const app = express();
@@ -28,7 +31,8 @@ app.get("/", (req, res) => {
 })
 
 // Books
-console.log(booksRouter instanceof express.Router);
+const log = "booksRouter instanceof express.Router";
+console.log(`${log}: ${booksRouter instanceof express.Router}`);
 app.use("/books", booksRouter);
 
 // Auth (login / register)
@@ -38,10 +42,11 @@ app.use("/auth", signUpRouter)
 // admin
 app.use("/admin", adminRouter)
 
-// Blog
+// Posts
 app.use("/post", blogRouter)
 
-// 자동입력 방지 보안 이미지 생성
+// GitHub
+app.use("/github", githubRouter)
 
-// 미들웨어
-// app.use(errorHandler)
+// 에러 핸들링 미들웨어
+app.use(errorHandler);
