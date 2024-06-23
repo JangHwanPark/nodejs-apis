dfa854b835409a842d821261a1152275220d9148
user <wkd5878@gmail.com>
2024-06-23 11:31:46 +0900

    refactoring: ejs 로 마이그레이션

--- src/server.js
+++ src/server.js
@@ -22,12 +22,39 @@ dotenv.config();         // 환경변수 읽어오기
 app.use(cors());         // 모든 프론트 서버 허용
 app.use(express.json()); // JSON 형식의 요청 본문을 파싱
 
+// ejs 를 사용자 뷰 엔진으로 설정
+app.set('view engine', 'ejs');
+
+// 뷰 파일의 디렉토리 설정
+app.set('views', path.join(__dirname, 'views'));
+
+// 정적파일 적용 - 정적 파일을 서빙할 디렉토리 설정
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Index page
 app.get("/", (req, res) => {
-    const indexPath = path.join(__dirname, "view", "index.html");
-    res.sendFile(indexPath, (err) => {
-        if (err) res.status(500).send(err);
-    });
+    const data = {
+        title: "학술",
+        message: "MOCK",
+        endpoints: [
+            { method: "GET", url: "http://34.197.44.20/admin/users", description: "전체 유저 조회", id: "user-count" },
+            { method: "GET", url: "http://34.197.44.20/books/books", description: "전체 책 조회", id: "book-count" },
+            { method: "GET", url: "http://34.197.44.20/coupang_products/all", description: "전체 쿠팡 제품 조회", id: "coupang-count" }
+        ],
+        specificEndpoints: [
+            { method: "GET", url: "http://34.197.44.20/admin/user/000043c2-b919-4281-9dab-f2a5b446973b", description: "특정 유저 조회" },
+            { method: "GET", url: "http://34.197.44.20/books/info/1", description: "특정 책 조회" },
+            { method: "GET", url: "http://34.197.44.20/coupang_products/product/1", description: "특정 제품 조회" },
+            { method: "GET", url: "http://34.197.44.20/coupang_products/food", description: "특정 카테고리 조회" }
+        ],
+        inputSections: [
+            { type: "users", label: "/admin/user/", placeholder: "유저의 ID를 입력하세요" },
+            { type: "books", label: "/books/info/", placeholder: "책의 ID를 입력하세요" },
+            { type: "products", label: "/coupang_products/product/", placeholder: "제품의 ID를 입력하세요" }
+        ]
+    };
+
+    res.render("index", data);
 });
 
 // Router
@@ -39,10 +66,6 @@ app.use("/coupang-products", coupangRouter);
 // Timeout
 app.use(timeoutMiddleware);
 app.use(retryMiddleware);
-
-// 정적파일 적용
-app.use(express.static(path.join(__dirname, "view")));
-app.set('view engine', 'ejs');
 
 // Start Server
 app.listen(process.env.PORT, () => {
