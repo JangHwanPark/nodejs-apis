40dc637ce41a965e015bdf9b5a7b6f24df4e4621
user <wkd5878@gmail.com>
2024-07-09 22:58:31 +0900

    refactoring: API 문서 디자인 변경

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -35,6 +35,20 @@ export const renderGuidePage = (req, res) => {
 export const renderCoupangPage = (req, res) => {
   const data = {
     message: "MOCK",
+    hero: 'Documentation',
+    mock: {
+      "pid": "6001",
+      "category": "Electronics",
+      "name": "Smartphone",
+      "base_price": "100000",
+      "price": "90000",
+      "unit_price": "90000",
+      "arrival": "2024-06-11",
+      "rating": "4.5",
+      "reward": "100",
+      "url": "http://example.com/product/123"
+    },
+    errorMessage: {"error": "상품정보를 찾을 수 없습니다."}
   };
   res.render('guide/coupang', data);
 }
@@ -42,6 +56,22 @@ export const renderCoupangPage = (req, res) => {
 export const renderBookPage = (req, res) => {
   const data = {
     message: "MOCK",
+    hero: 'Books',
+    mock: {
+      "book_id": 2,
+      "name": "예약판매 최고의 프로덕트는 무엇이 다른가",
+      "category": "Computer Science",
+      "author": "공옥순",
+      "publisher": "2024.05.31",
+      "discount": "10%",
+      "price": "14,850원",
+      "price_normal": "정가16,500원",
+      "point": "820p",
+      "description": "소",
+      "review": "0.0",
+      "img": "kyobo/book_img_002.jpg"
+    },
+    errorMessage: {"error": "도서정보를 찾을 수 없습니다."}
   };
   res.render('guide/books', data);
 }
@@ -49,6 +79,22 @@ export const renderBookPage = (req, res) => {
 export const renderUserPage = (req, res) => {
   const data = {
     message: "MOCK",
+    hero: 'Users',
+    guideTitle: 'EndPoint - GET users/v1/get/all',
+    mock: {
+      "uid": "123456",
+      "name": "홍길동",
+      "age": 25,
+      "city": "서울",
+      "email": "hong@example.com",
+      "phone": "010-1234-5678",
+      "gender": "남성",
+      "occupation": "개발자",
+      "join_date": "2023-06-07",
+      "address": "서울특별시 강남구",
+      "password": "securepassword123"
+    },
+    errorMessage: {"error": "사용자를를 찾을 수 없습니다"}
   };
   res.render('guide/users', data);
 }
